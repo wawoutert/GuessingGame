3e711ccbd30ee808e6c72a5ff8ddd84054496e01
wawoutert <wawmulder@gmail.com>
2020-06-11 00:14:30 +0200

    function in guessinggame and addition of readme and makefile

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guessing Game
+This file was created on: do 11 jun 2020  0:08:24 CEST
+The guessing game program consists of 28 lines
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,15 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+function hint {
+  if [[ $1 -gt $2 ]]
+  then
+    echo "Your guess is too high, please try again"
+  else
+    echo "Your guess is too low, please try again"
+  fi
+}
+
 terminate=0
 answer=$(ls | wc -l)
 
@@ -6,16 +18,11 @@ do
   echo "How many files are in the current directory?"
   read guess
   echo "You guessed $guess"
-#  echo "The answer was $answer"
-  
   if [[ $guess -eq $answer ]]
   then
     echo "Congratulations, you are correct!"
     let terminate=1
-  elif [[ $guess -gt $answer ]]
-  then
-    echo "Your answer is too high, please try again"
-  else
-    echo "Your answer is too low, please try again"
+      else
+    echo $(hint $guess $answer) #without echo, command is not found
   fi
 done
