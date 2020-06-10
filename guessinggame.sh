ddce42a646023144135f2fc0fe6ff9f1ecd0fa39
wawoutert <wawmulder@gmail.com>
2020-06-10 23:02:18 +0200

    Included the while loop and made it work

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,13 +1,21 @@
-echo "How many files are in the current directory?"
-read guess
-
+terminate=0
 answer=$(ls | wc -l)
-echo "You guessed $guess"
-echo "The answer was $answer"
 
-if [[ $guess -eq $answer ]]
-then
-  echo "You are correct!"
-else 
-  echo "Please try again."
-fi
+while [[ $terminate -eq 0 ]]
+do
+  echo "How many files are in the current directory?"
+  read guess
+  echo "You guessed $guess"
+#  echo "The answer was $answer"
+  
+  if [[ $guess -eq $answer ]]
+  then
+    echo "Congratulations, you are correct!"
+    let terminate=1
+  elif [[ $guess -gt $answer ]]
+  then
+    echo "Your answer is too high, please try again"
+  else
+    echo "Your answer is too low, please try again"
+  fi
+done
